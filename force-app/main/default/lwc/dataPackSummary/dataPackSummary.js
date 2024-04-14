0255e7797b5b5b2ac36d6df22c0a614ce0dd0834
Ronaldo Garcia <ronaldo.garcia@ronaldo-ltm6ykd.internal.salesforce.com>
2024-04-14 08:11:10 -0300

    adjusted the layout for data pack summary.

--- force-app/main/default/lwc/dataPackSummary/dataPackSummary.js
+++ force-app/main/default/lwc/dataPackSummary/dataPackSummary.js
@@ -4,6 +4,8 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class DataPackSummary extends LightningElement {
 
+    showSpinner = false;
+
     @api objects;
 
     handleToggleSection(event) {
@@ -11,26 +13,31 @@ export default class DataPackSummary extends LightningElement {
     }
 
     saveDataPack() {
+        this.showSpinner = true;
         console.log('Enter saveDataPack');
         console.log('Value of input ---> '+JSON.stringify(this.objects));
         saveDataPackRecords({ dataPackJson: JSON.stringify(this.objects) })
             .then(result => {
                 console.log('Records saved successfully --> '+JSON.stringify(result));
                 this.showToast('Success', 'Data Pack saved successfully', 'success');
+                this.showSpinner = false;
             })
             .catch(error => {
                 console.log('Got error --> '+JSON.stringify(error));
                  this.showToast('Error', 'Error in saving Data Pack', 'error');
+                 this.showSpinner = false;
             })
     }
 
     showToast(title, message, variant) {
+        
         const toastEvent = new ShowToastEvent({
              title: title,
              message: message,
              variant: variant
         });
         this.dispatchEvent(toastEvent);
+        
     }
 
 
